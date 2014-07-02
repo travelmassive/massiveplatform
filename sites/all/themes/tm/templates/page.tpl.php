5870fe2c4e6c66f55eb305d099f9eef2fe8fed9d
Olaf Veerman <olaf.veerman@gmail.com>
2014-07-02 17:00:02 +0100

    Add header, section and column to page layout.

--- sites/all/themes/tm/templates/page.tpl.php
+++ sites/all/themes/tm/templates/page.tpl.php
@@ -58,20 +58,25 @@
   <main id="main" role="main">
     <div class="row">
       <div id="content" role="main">
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?>
-          <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php print $messages; ?>
-        <?php print render($tabs); ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
+        <section>
+          <header>
+            <a id="main-content"></a>
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+              <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php print $messages; ?>
+            <?php print render($tabs); ?>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?>
+              <ul class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+          </header>
+          <div class="column">
+            <?php print render($page['content']); ?>
+            <?php print $feed_icons; ?>
+          </div>
       </div>
 
     </div>

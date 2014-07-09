9bc32f54080817683d35569b487b53843634cbe9
Daniel Silva <daniel.silva@flipside.org>
2014-07-09 18:02:54 +0100

    New homepage markup

--- sites/all/modules/custom/tm_base/templates/tm_base_homepage.tpl.php
+++ sites/all/modules/custom/tm_base/templates/tm_base_homepage.tpl.php
@@ -6,16 +6,17 @@
 global $user;
 ?>
 
-<section id="homepage">
-	<header>
+<section id="welcome">
+	<header class="hd">
 		<h1 class="prime-title"><?php print check_plain($homepage_title) ?></h1>
 		<p class="subtitle"><?php print check_plain($homepage_description) ?></p>
-		<?php if ($homepage_stats_block) : ?>
-		  <?php print render($homepage_stats_block); ?>
-		<?php endif; ?>
-		<?php if (!$user->uid): ?>
-		<p class="cta-wrapper"><?php print l(t('Join'), 'user/register', array('attributes' => array('class' => 'bttn bttn-secondary bttn-xl'))) ?></p>
-		<?php endif; ?>
 	</header>
-	Bloco do Olaf!
+	<div class="bd">
+  	<?php if ($homepage_stats_block) : ?>
+  	  <?php print render($homepage_stats_block); ?>
+  	<?php endif; ?>
+  	<?php if (!$user->uid): ?>
+  	<p class="cta-wrapper"><?php print l(t('Join'), 'user/register', array('attributes' => array('class' => 'bttn bttn-secondary bttn-xl'))) ?></p>
+  	<?php endif; ?>
+	</div>
 </section>

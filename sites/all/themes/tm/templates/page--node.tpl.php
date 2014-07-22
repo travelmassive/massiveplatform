e8a85ddd37160e86b6e1d12a3388080a279dd42a
Olaf Veerman <olaf.veerman@gmail.com>
2014-07-22 15:05:50 +0100

    Remove the tabs from the page templates. Fix #189

--- sites/all/themes/tm/templates/page--node.tpl.php
+++ sites/all/themes/tm/templates/page--node.tpl.php
@@ -65,7 +65,7 @@
         <?php print render($title_suffix); ?>
         <?php print render($page['header']); ?>
         <?php print $messages; ?>
-        <?php print render($tabs); ?>
+        <?php //print render($tabs); ?>
         <?php print render($page['help']); ?>
         <?php if ($action_links): ?>
           <ul class="action-links"><?php print render($action_links); ?></ul>

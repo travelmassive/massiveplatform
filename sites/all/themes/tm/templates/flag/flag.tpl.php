530d49ca1861901e28e3706142a62c9fab7f4dee
travelmassive <travelmassive@localdev.travelmassive.com>
2014-10-23 15:45:24 +1100

    Cleanup flag displays again add demo user

--- sites/all/themes/tm/templates/flag/flag.tpl.php
+++ sites/all/themes/tm/templates/flag/flag.tpl.php
@@ -39,25 +39,28 @@
  * advanced theming you may have to remove all the whitespace.
  */
 ?>
-<?php if (!isset($hide_flag) || $hide_flag == FALSE) : ?>
+
+
+<?php if (isset($event_flag) && $event_flag == "show_closed") : ?>
 
 <li class="<?php print $flag_wrapper_classes; ?>">
-	<?php if (isset($approved_members_only) and ($action == "flag")): ?>
+  <span class="follow bttn bttn-secondary bttn-m disabled" rel="nofollow">Past Event </span>
+</li>
+
+<?php elseif (isset($event_flag) && $event_flag == "show_not_approved") : ?>
+
+<li class="<?php print $flag_wrapper_classes; ?>">
     <span onClick="alert('You need to be an Approved Member to register for this event.');" class="follow bttn bttn-secondary bttn-m <?php if ($status == 'flagged'): ?>on<?php endif; ?> <?php print $flag_classes ?>" rel="nofollow"><?php print $link_text; ?></span>
-  <?php elseif ($link_href): ?>
+</li>
+
+<?php elseif (!isset($hide_flag) || $hide_flag == FALSE) : ?>
+
+<li class="<?php print $flag_wrapper_classes; ?>">
+	<?php if ($link_href): ?>
     <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="follow bttn bttn-secondary bttn-m <?php if ($status == 'flagged'): ?>on<?php endif; ?> <?php print $flag_classes ?>" rel="nofollow"><span><?php print $link_text; ?></span></a>
   <?php else: ?>
     <span class="<?php print $flag_classes ?>"><?php print $link_text; ?></span>
   <?php endif; ?>
-</li>
-
-<?php else: ?>
-
-<li class="<?php print $flag_wrapper_classes; ?>">
-	<?php if ((isset($display_external) and ($display_external == 0)) 
-            and (isset($display_closed) and ($display_closed == TRUE) and ($action == "flag"))): ?>
-	    <span class="follow bttn bttn-secondary bttn-m disabled" rel="nofollow">Past Event </span>
-	<?php endif; ?>
 </li>
 
 <?php endif; ?>

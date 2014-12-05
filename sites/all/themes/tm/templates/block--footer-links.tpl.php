5bc61483bc0de7ed944e089f2f2b1bb724f8db3c
travelmassive <travelmassive@localdev.travelmassive.com>
2014-12-06 02:14:45 +1100

    Add footer link template

---
+++ sites/all/themes/tm/templates/block--footer-links.tpl.php
@@ -0,0 +1,10 @@
+<section class="foot">
+	<h2>More Massive</h2>
+		<ul class="links">
+		<li class="menu first"><a href="/content/about">About</a></li>
+		<li class="menu-769"><a href="/content/contacts" class="active-trail active">Contacts</a></li>
+		<li class="menu-770"><a href="/content/help">Help</a></li>
+		<li class="menu-771"><a href="/content/press-kit">Press kit</a></li>
+		<li class="menu-583 last"><a href="/sponsors">Sponsors</a></li>
+	</ul>
+</section>

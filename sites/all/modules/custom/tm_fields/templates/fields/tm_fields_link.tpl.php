443d2efce7c9d9d506721c0f964ce0455ab73d7a
travelmassive <travelmassive@localdev.travelmassive.com>
2014-12-31 04:30:55 +1100

    Remove validation from links as forcing http:// on users is annoying. Instead do validation in tm_fields_link.tpl.php

--- sites/all/modules/custom/tm_fields/templates/fields/tm_fields_link.tpl.php
+++ sites/all/modules/custom/tm_fields/templates/fields/tm_fields_link.tpl.php
@@ -2,6 +2,17 @@
 
 $display_url = $url;
 
+// check url
+$render_url = true;
+if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
+	$render_url = false;
+}
+
+// add http when it's not given
+if (strpos("http", $url) === 0) {
+	$url = "http://" . $url; // add http://
+}
+
 // remove http and https
 $display_url = str_ireplace("http://", "", $display_url);
 $display_url = str_ireplace("https://", "", $display_url);
@@ -11,5 +22,9 @@ if (substr_count($display_url, "/") == 1) {
 	$display_url = rtrim($display_url, '/');
 }
 
+if ($render_url) {
 ?>
 <a href="<?php print $url; ?>" rel="nofollow" target="_blank"><?php print $display_url; ?></a>
+<?php } else { ?>
+&nbsp;
+<?php } ?>

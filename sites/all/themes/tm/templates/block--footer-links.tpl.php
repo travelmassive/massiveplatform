13bf8f92e24dcec565eb762c0193badef4ccf2e1
travelmassive <travelmassive@localdev.travelmassive.com>
2014-12-30 16:43:57 +1100

    Update contact page

--- sites/all/themes/tm/templates/block--footer-links.tpl.php
+++ sites/all/themes/tm/templates/block--footer-links.tpl.php
@@ -2,7 +2,7 @@
 	<h2>More Massive</h2>
 		<ul class="links">
 		<li class="menu first"><a href="/content/about-travel-massive">About</a></li>
-		<li class="menu-770"><a href="/content/contact-us" class="active-trail active">Contact</a></li>
+		<li class="menu-770"><a href="/contact" class="active-trail active">Contact</a></li>
 		<li class="menu-770"><a href="/blog/start-a-chapter">Start A Chapter</a></li>
 		<li class="menu-770"><a href="/blog/media-kit">Media kit</a></li>
 		<li class="menu-770"><a href="/sponsors">Sponsors</a></li>

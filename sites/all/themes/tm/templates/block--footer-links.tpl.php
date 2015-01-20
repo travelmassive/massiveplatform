0663cc60fe9b7b903dabecbefd4d17af614ac568
travelmassive <travelmassive@localdev.travelmassive.com>
2015-01-20 23:21:59 +1100

    Fix broken following limit

--- sites/all/themes/tm/templates/block--footer-links.tpl.php
+++ sites/all/themes/tm/templates/block--footer-links.tpl.php
@@ -1,7 +1,7 @@
 <section class="foot">
 	<h2>More Massive</h2>
 		<ul class="links">
-		<li class="menu first"><a href="https://travelmassive.com/blog/about-travel-massive/">About</a></li>
+		<li class="menu first"><a href="/blog/about-travel-massive/">About</a></li>
 		<li class="menu-770"><a href="/contact" class="active-trail active">Contact</a></li>
 		<li class="menu-770"><a href="/blog/start-a-chapter">Start A Chapter</a></li>
 		<li class="menu-770"><a href="/blog/media-kit">Media kit</a></li>

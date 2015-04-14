d64f19749ded594b3c5099c4460d660a8e3c7af1
travelmassive <travelmassive@localdev.travelmassive.com>
2015-04-14 16:40:24 +1000

    CSS tweaks to front page and header margin

--- sites/all/modules/custom/tm_base/templates/tm_base_homepage.tpl.php
+++ sites/all/modules/custom/tm_base/templates/tm_base_homepage.tpl.php
@@ -7,7 +7,7 @@ global $user;
 ?>
 
 <section id="welcome">
-	<header class="hd">
+	<header class="hd" style="margin-top: 3em;">
 		<h1 class="prime-title"><?php print check_plain($homepage_title) ?></h1>
 		<p class="subtitle"><?php print check_plain($homepage_description) ?></p>
 	</header>

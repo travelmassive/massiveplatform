74042f98b4d05a63ab1e2b56becdcb14b5cf0784
travelmassive <travelmassive@localdev.travelmassive.com>
2015-06-26 01:42:24 +1000

    Add community values link to footer

--- sites/all/themes/tm/templates/block--footer-links.tpl.php
+++ sites/all/themes/tm/templates/block--footer-links.tpl.php
@@ -3,6 +3,7 @@
 		<ul class="links">
 		<li class="menu first"><a href="/blog/about-travel-massive/">About</a></li>
 		<li class="menu-770"><a href="/contact" class="active-trail active">Contact</a></li>
+		<li class="menu-770"><a href="/blog/community-values/" class="active-trail active">Community Values</a></li>
 		<li class="menu-770"><a href="/blog/start-a-chapter-guide/">Start A Chapter</a></li>
 		<li class="menu-770"><a href="/blog/media-kit/">Media kit</a></li>
 		<li class="menu-770"><a href="/sponsors">Sponsors</a></li>

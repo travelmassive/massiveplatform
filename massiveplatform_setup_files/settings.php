cecb3018d1077fc687a92524878b90c104a690e2
travelmassive <travelmassive@localdev.travelmassive.com>
2015-08-24 01:02:57 +1000

    Event fixes

--- massiveplatform_setup_files/settings.php
+++ massiveplatform_setup_files/settings.php
@@ -723,7 +723,7 @@ $conf["tm_notification_moderation_non_community_message"] = 'To be a member of t
 /** Event types **/
 $conf['tm_event_types'] = array("community" => "Community", "workshop" => "Workshop", "conference" => "Conference");
 $conf['tm_event_types_plural'] = array("community" => "Community", "workshop" => "Workshops", "conference" => "Conferences");
-$conf['tm_event_types_tips'] = array("workshop" => "Learn and share with other members around the world. Have an idea for a workshop you would like to attend or host? Please <a href='/contact'>let us know</a>.", "conference" => "We're proud community partners with the world's leading travel industry conferences and exhibitions.");
+$conf['tm_event_types_tips'] = array("workshop" => "Learn and share with other members around the world. have an idea for a workshop you would like to attend or host? Please <a href='/contact'>let us know</a>.", "conference" => "We're proud community partners with the world's leading travel industry conferences and exhibitions. <a href='/blog/about-travel-massive'>Learn more</a>");
 $conf['tm_event_types_default'] = "community";
 
 $conf['install_profile'] = 'tm';

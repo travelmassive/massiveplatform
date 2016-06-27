f1dda60970091f9a31515b1d17e788c3ed2cf636
travelmassive <travelmassive@localdev.travelmassive.com>
2016-06-27 12:38:50 +1000

    Update tm_status_updates: improve responsive design

--- sites/all/modules/custom/tm_status_updates/templates/tm_status_updates_post_form.tpl.php
+++ sites/all/modules/custom/tm_status_updates/templates/tm_status_updates_post_form.tpl.php
@@ -1,22 +1,28 @@
 <style>
-.tm-status-update-container { padding-top: 12px; }
-.tm-status-update-image-container { left: 8px; top: 0px; width: 100px;}
+.tm-status-update-form-container { padding-top: 12px; }
+.tm-status-update-image-container { left: 8px; top: 0px; width: 100px; }
 .tm-status-update-image { width: 48px; border-radius: 4px; }
 .tm-status-update-text-container { position: absolute; left: 72px; right: 16px; top: 12px; }
 .tm-status-update-post-actions { text-align: right; margin-top: 6px; }
 #tm-status-update-post-as { margin-right: 8px; }
-#charNum { font-size: 14px; color: #888; margin-right: 8px; position: absolute; top: -16px; right: 8px;}
+#charNum { font-size: 14px; color: #888; margin-right: 8px; position: absolute; top: -16px; right: 8px; }
 #tm-status-update-feedback-container { font-size: 14px; margin-bottom: -12px; min-height: 28px; }
 #tm-status-update-location-text { font-size: 14px; color: #213040; text-decoration: underline; }
 #tm-status-update-feedback-loading-image { margin-top: 6px; padding-right: 8px; }
 #tm-status-update-text { resize: none; }
+
+#tm-status-update-location-container { float: left; min-width: 150px; min-height: 2rem; text-align: left; margin-left: 56px; }
+@media only screen and (max-width: 650px) {
+	#tm-status-update-location-container { float: right; min-width: 300px; text-align: right; margin-left: 0px; }
+}
+
 </style>
 
 <script>
 var tm_update_post_as_images = __POST_AS_IMAGES__;
 </script>
 
-<div class="tm-status-update-container">
+<div class="tm-status-update-form-container">
 
 	<span class="tm-status-update-image-container">
 		<div style="float: left; height: 56px; width: 0px;"></div>
@@ -32,7 +38,11 @@ var tm_update_post_as_images = __POST_AS_IMAGES__;
 	<div class="tm-status-update-post-actions">
 		
 		<span id="charNum">&nbsp;</span>
-		<span id='tm-status-update-location-field' class='tm-status-update-location' style='display: none;'><span id='tm-status-update-location-text' onClick='tm_user_status_enter_location()'>Add location</span></span>
+		<div id="tm-status-update-location-container">
+			<span id='tm-status-update-location-field' class='tm-status-update-location' style='display: none;'>
+				<span id='tm-status-update-location-text' onClick='tm_user_status_enter_location()'>Add location</span>
+			</span>
+		</div>
 		<select id="tm-status-update-post-as" name="tm-status-update-post-as" style="margin-bottom: 8px;">
 			__POST_AS_OPTIONS__
 		</select>
@@ -48,7 +58,6 @@ var tm_update_post_as_images = __POST_AS_IMAGES__;
 	</div>
 	<div id="tm-status-update-link-preview-html"></div>
 </div>
-
 
 <div style="clear: both;"></div>
 

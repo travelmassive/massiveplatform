0921b58f1b6c27faf685057d5447bbcc22dae858
travelmassive <travelmassive@localdev.travelmassive.com>
2016-07-09 20:40:15 +1000

    Update tm_payments: add billing address option. Set default earlybird time if date selected

--- sites/all/modules/custom/tm_events/js/tm_events_edit_event.js
+++ sites/all/modules/custom/tm_events/js/tm_events_edit_event.js
@@ -20,7 +20,13 @@
 		if ((tm_events_form_subtitle != null) && (tm_events_form_heading != null)) {
 			$("#page-title").html('<em>' + tm_events_form_subtitle + '</em>' + tm_events_form_heading);
 		}
-		
 	}
+
+	// If early bird date selected, give the time field a default if not already set
+	$("#edit-field-event-payment-early-date-und-0-value-datepicker-popup-0").change(function() {
+		if ($("#edit-field-event-payment-early-date-und-0-value-timeEntry-popup-1").val() == "") {
+			$("#edit-field-event-payment-early-date-und-0-value-timeEntry-popup-1").val("12:00");
+		}
+	});
 
 });})(jQuery, Drupal, this, this.document);

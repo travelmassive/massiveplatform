22cfa8fa3399e32a4408b1fdd6f404016c2735bc
travelmassive <travelmassive@localdev.travelmassive.com>
2016-09-04 08:07:20 +1000

    Update sample config

--- massiveplatform_setup_files/settings.php
+++ massiveplatform_setup_files/settings.php
@@ -79,7 +79,7 @@ $conf['tm_event_member_event_min_followers'] = 5; // how many followers a member
 $conf['tm_event_send_announcements_intro'] = "Get more people to your event by sending a great announcement. See our guide below."; // tip
 $conf['tm_event_announcement_enable_cover_image'] = true; // enable including event cover image in announcement email
 $conf['tm_event_online_timezones'] = array("America/Los_Angeles" => "Los Angeles", "America/New_York" => "New York", "Europe/London" => "London", "Europe/Athens" => "Athens", "Asia/Singapore" => "Singapore", "Australia/Sydney" => "Sydney"); // display online event date in these timezones
-$conf['tm_event_title_placeholder_text'] = 'Write a clear and engaging event title to make people interested and excited!'; // placeholder text for event title
+$conf['tm_event_title_placeholder_text'] = 'Write a clear and engaging event title'; // placeholder text for event title
 $conf['tm_event_description_placeholder_text'] = 'Describe how fantastic your event will be and why people should attend.'; // placeholder text for event description
 $conf['tm_event_follow_company'] = true; // follow company when joining company event
 $conf['tm_event_follow_member'] = true; // follow member when joining member event

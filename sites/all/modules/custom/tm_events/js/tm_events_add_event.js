423f63f3916fcf83419832ae1a096b60bc4d688f
travelmassive <travelmassive@localdev.travelmassive.com>
2016-09-04 17:22:34 +1000

    Update tm_events: improve back button detetion for firefox and safari

--- sites/all/modules/custom/tm_events/js/tm_events_add_event.js
+++ sites/all/modules/custom/tm_events/js/tm_events_add_event.js
@@ -1,7 +1,9 @@
 /* Add edit methods */
+var checkLastEdit = null;
+var check_last_edited_notified = false;
 (function ($, Drupal, window, document, undefined) {jQuery(document).ready(function(){
 
-	function getCookie(cname) {
+	getCookie = function(cname) {
 	    var name = cname + "=";
 	    var ca = document.cookie.split(';');
 	    for(var i = 0; i <ca.length; i++) {
@@ -16,21 +18,42 @@
 	    return "";
 	}
 
-	var last_edited_event = getCookie("Drupal.visitor.last_edited_event");
-	if (last_edited_event != "") {
-		
-		setTimeout(function() {
-			confirm_title = "Edit event";
-			message = "Would you like to edit the event you just created?";
-			$.prompt(message, { buttons: { "Yes, edit my event": true, "Close": false },
-				title: confirm_title,
-				submit: function(e,v,m,f){
-					if (v == true) {
-						window.location = "/node/" + last_edited_event + "/edit";
+	checkLastEdit = function() {
+
+		var last_edited_event = getCookie("Drupal.visitor.last_edited_event");
+		if ((last_edited_event != "") && (!check_last_edited_notified)) {
+
+			check_last_edited_notified = true; // prevent showing multiple times
+
+			setTimeout(function() {
+				confirm_title = "Edit event";
+				message = "Would you like to edit the event you just created?";
+				$.prompt(message, { buttons: { "Yes, edit my event": true, "Close": false },
+					title: confirm_title,
+					submit: function(e,v,m,f){
+						if (v == true) {
+							window.location = "/node/" + last_edited_event + "/edit";
+						}
 					}
-				}
-			});
-		}, 50);
+				});
+			}, 50);
+		}
 	}
 
+	// Chrome, IE will load this on back
+	checkLastEdit();
+
+	// Force Firefox to load js on page back
+	// http://stackoverflow.com/questions/2638292/after-travelling-back-in-firefox-history-javascript-wont-run
+	window.onload = function(){};
+	window.onunload = function(){};
+
 });})(jQuery, Drupal, this, this.document);
+
+// Detect back in Safari
+// http://stackoverflow.com/questions/11979156/mobile-safari-back-button
+window.onpageshow = function(event) {
+    if (event.persisted) {
+        checkLastEdit();
+    }
+};

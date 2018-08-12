453d26fd4e1672fa1e1162ce859ba7c76607fc51
root <ian@travelmassive.com>
2018-08-12 12:48:13 +0000

    Update tm_chapters, tm_flags: allow chapter leaders to add member to their chapter

---
+++ sites/all/modules/custom/tm_chapters/js/tm_chapters_moderation.js
@@ -0,0 +1,37 @@
+/* Chapter moderation methods */
+(function ($, Drupal, window, document, undefined) {jQuery(document).ready(function(){
+
+  // allow chapter leader to add a member to their chapter
+  jq_add_member_to_chapter = function(uid, name) {
+    
+    countChar = function(val) {
+      var len = val.value.length;
+      if (len >= 150) {
+        val.value = val.value.substring(0, 150);
+      } else {
+        $('#charNum').text((150 - len) + " chars");
+      }
+    };
+
+    name = decodeURIComponent(name);  
+
+    var message = "Select chapter &mdash; <div id='moderation_chapters_list' style='display: inline-block'>Loading...</div>";
+    message = message + "<p>You can add a short message. <textarea id='form_moderator_message' onkeyup='countChar(this);' value='' placeholder='Welcome to our chapter...' rows='2' cols='50'></textarea><div style='margin-top: -16px; float: right;'><span id='charNum' style='font-size: 10pt; color: #888;'>150 chars</span></div></p>",
+
+    $.prompt(message, { 
+      buttons: { "Add to chapter": true, "Cancel": false },
+      title: "Add " + name + " to chapter?",
+      loaded: function() {
+        // load available chapters list
+        $("#moderation_chapters_list").load("/chapters/moderation-chapter-list-ajax/" + uid + "/add");
+      },
+      submit: function(e,v,m,f){
+        if (v == true) {
+          window.location = '/chapters/moderation-chapter-add-member/' + uid + '/' + $("#moderation_chapter_ids option:selected").val() + '?message=' + $("#form_moderator_message").val();
+        }
+      }
+    });
+
+  }
+
+});})(jQuery, Drupal, this, this.document);

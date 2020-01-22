630a8587602d7234f9e0d788ac2dc8ac7415ade6
root <ian@travelmassive.com>
2020-01-22 14:10:35 +0000

    Update tm_base: add tm_base_parsedown(). Update tm_lists: add markdown support

--- sites/all/modules/custom/tm_lists/templates/tm_list_view_item.tpl.php
+++ sites/all/modules/custom/tm_lists/templates/tm_list_view_item.tpl.php
@@ -1,7 +1,7 @@
 <div class='tm-list-view-item-container'>
 	<span class='tm-list-view-item-title'>__ITEM_POSITION____ITEM_SCORE__<h2>__ITEM_TITLE__</h2></span>
 	<span class='tm-list-view-item-image'><a target='_blank' href='__PROFILE_URL__'><img class='tm-list-item-__ITEM_IMAGE_CLASS__' src='__ITEM_IMAGE_URL__'></a></span>
-	<span class='tm-list-view-item-comment'><p>__ITEM_COMMENT__</p>
+	<span class='tm-list-view-item-comment'>__ITEM_COMMENT__
 	<span class='tm-list-view-item-links'>__ITEM_LINKS__</span>
 	</span>
 </div>

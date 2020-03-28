34e626384331b5ab904f0d58f42c378a5f08ec88
root <ian@travelmassive.com>
2020-03-28 11:55:45 +0000

    Update tm_lists: add featured item link

---
+++ sites/all/modules/custom/tm_lists/templates/tm_list_feature_item.tpl.php
@@ -0,0 +1,57 @@
+<div class='tm-list-items'>
+<ul class='tm-list-items-list featured'>
+__LIST_ITEM__
+</ul>
+</div>
+
+<style>
+
+/* Featured */
+
+.tm_list_featured .jqi { 
+	width: 90% !important;
+	max-width: 1150px;
+	top: 250px !important;
+	margin-left: auto !important;
+	margin-right: auto !important;
+	left: 0 !important;
+	right: 0 !important;
+	background: none !important;
+	border: none !important;
+	padding: 0 !important;
+}
+
+.tm_list_featured .jqimessage {
+	padding: 0 !important;
+}
+
+.tm_list_featured .jqiform, .tm_list_featured .jqistate { 
+	background: none !important;
+}
+
+.tm_list_featured .jqiclose {
+	display: none;
+}
+
+.tm-list-items-list {
+	filter: blur(4px);
+}
+
+.tm-list-items-list.featured {
+	filter: blur(0px);
+}
+
+.tm_list_featured .tm-list-item-actions-container {
+	display: none;
+}
+
+</style>
+
+<script>
+// scroll to top
+window.scroll({
+  top: 0, 
+  left: 0, 
+  behavior: 'smooth'
+});
+</script>

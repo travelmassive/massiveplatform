5cc7bb28bc96aff3d92e9108e380a05da1718d9b
root <ian@travelmassive.com>
2020-04-01 06:04:04 +0000

    Update

--- sites/all/modules/custom/tm_match/js/tm_match_display.js
+++ sites/all/modules/custom/tm_match/js/tm_match_display.js
@@ -86,7 +86,7 @@
 		  pullDeltaX = 0;
 		  animating = false;
 		  if (!tm_match_fetching_new_cards) {
-		  	jQuery(".tm_match__card__more_link").delay(1000).fadeIn();
+		  	jQuery(".tm_match__card__more_link").delay(500).fadeIn();
 		  }
 		}, 300);
 

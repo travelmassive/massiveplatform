073633de2e7ec76933b24af71863ac5fef02ac55
root <ian@travelmassive.com>
2020-04-13 07:17:11 +0000

    Update tm_match: show connections

--- sites/all/modules/custom/tm_match/templates/tm_match_card.tpl.php
+++ sites/all/modules/custom/tm_match/templates/tm_match_card.tpl.php
@@ -1,4 +1,4 @@
-<div class="tm_match__card" data-swipe-enabled="1" data-card-uid="__UID__" data-card-first-name="__FIRST_NAME__" data-is-last-card="__IS_LAST_CARD__" data-follows-you-back="__FOLLOWS_YOU_BACK__">
+<div class="tm_match__card follows-back-__FOLLOWS_YOU_BACK__" data-swipe-enabled="1" data-card-uid="__UID__" data-card-first-name="__FIRST_NAME__" data-is-last-card="__IS_LAST_CARD__" data-follows-you-back="__FOLLOWS_YOU_BACK__">
 	<div class="tm_match__card__top __CSS_COLOR__">
 		<div class="tm_match__card__img" style="background-image: url('__IMAGE_URL__');"></div>
 		<p class="tm_match__card__name">__NAME__</p>

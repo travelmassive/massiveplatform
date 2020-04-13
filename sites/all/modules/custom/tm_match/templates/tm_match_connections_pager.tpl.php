3f678e666eaaea53b25f1fb95ecac104e23e0493
root <ian@travelmassive.com>
2020-04-13 07:15:32 +0000

    Update tm_match: show connections

---
+++ sites/all/modules/custom/tm_match/templates/tm_match_connections_pager.tpl.php
@@ -0,0 +1,10 @@
+<div id="connections-list-load-more">
+	<ul class="pager pager-load-more" style="display:block;">
+		<li class="pager-next first last">
+			<a href="javascript:void(0);" id="connections-load-more">Show more</a>
+			<div style="min-height: 20px; margin-top: 4px;">
+				<img class="connections load-more-spinner" style="display: none;" src='/sites/all/themes/tm/images/load-more-ajax-loader-2.gif'>
+			</span>
+		</li>
+	</ul>
+</div>
---
+++ sites/all/modules/custom/tm_match/templates/tm_match_user.tpl.php
@@ -0,0 +1,24 @@
+<article typeof="sioc:UserAccount" class="card contained user-profile view-mode-grid clearfix">
+	<a href="/user/__UID__">
+		<div class="media">
+			<div class="cover">
+				<img typeof="foaf:Image" src="__COVER_IMAGE_URL__" width="400" height="225" alt="">
+			</div>
+			<div class="avatar">
+				<span class="badge-user">
+					<img typeof="foaf:Image" src="__AVATAR_URL__" width="100" height="100" alt="">
+				</span>
+			</div>
+		</div>
+		<div class="teaser">
+			<h1 class="prime-title">__FULL_NAME__</h1>
+			<p class="meta">
+				__JOB_ROLE__
+				__ORGANIZATION_NAME__
+			</p>
+		</div>
+	</a>
+	<div class="extra">
+		<a class="chapter-sticker" href="__CHAPTER_URL__" style="background-color: __CHAPTER_COLOR__">__CHAPTER_CODE__</a>
+	</div>
+</article>
